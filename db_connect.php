98d0ea9f93466715509ad33e6efc363888eb16e2
shivamsoni-in <152253499+shivamsoni-in@users.noreply.github.com>
2023-11-28 13:55:12 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,1 @@
-<?php
-
-    $conn = new mysqli("localhost","iiijssmy_mahesh","Maheshsurvey@07","iiijssmy_hairhealthandwellness");
-    
-    // Check connection
-    if ($conn -> connect_errno) {
-      echo "Failed to connect to MySQL: " . $conn -> connect_error;
-      exit();
-    }
-    
-?>
+404
